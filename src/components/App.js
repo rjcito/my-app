3103cd0b4939afa24547b3ca8ff394ff08ee14cc
Roberto Jacquez <rjqz92@gmail.com>
2023-08-21 20:59:01 -0600

    Added dark mode feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NavBar from "./NavBar";
 import Home from "./Home";
 import QuoteForm from "./QuoteForm"
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme ('dark');
+    }else {
+      setTheme('light');
+    }
+  }
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
   return (
+
     <Router>
     <div className ="App">
       <NavBar />
@@ -18,7 +32,7 @@ function App() {
             <QuoteForm /> 
           </Route>
         </Switch>
-        
+        <button onClick ={toggleTheme}>Dark Mode</button>
       </div>
     </div>
     </Router> 
